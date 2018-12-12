d411d9bb45599c561fa58d2ee2190252b15002e5
Seth Parker <sparker1491@gmail.com>
2018-12-11 16:09:21 -0800

    Modified HarvestActions to no longer give any base production. HarvestActions which have a resourceCost (which will always comprise exclusively refined resources) may still generate a baseProduction

--- EvoMod2/EvoMod2/HarvestAction.cs
+++ EvoMod2/EvoMod2/HarvestAction.cs
@@ -11,6 +11,11 @@ namespace EvoMod2
 	{
 		public HarvestAction(int totalResourceCount, Random random, Vector localResourceLevels) : base(totalResourceCount)
 		{
+			/*
+			 * Initializes baseProduction in order to determine resource production priorities.
+			 * Fills out localResourceLevelsProductionModifier according to production priorities and local resource levels.
+			 * Clears baseProduction. This is done to make "HarvestAction" results more strictly tied to location.
+			 */
 			localResourceLevels.Magnitude = 1.0f;
 			for (int i = 0; i < DisplayForm.NaturalResourceTypesCount; i++)
 			{
@@ -18,10 +23,7 @@ namespace EvoMod2
 				{
 					baseProduction[i] = 1.0f - (float)random.NextDouble();
 				}
-				localResourcesDecision[i] = 0.0f;
-				inventoryResourcesDecision[i] = 0.0f;
 			}
-
 			for (int i = 0; i < DisplayForm.NaturalResourceTypesCount; i++)
 			{
 				localResourceLevelsProductionModifier[i] = new MatrixMath.Vector(DisplayForm.NaturalResourceTypesCount);
@@ -29,13 +31,28 @@ namespace EvoMod2
 				{
 					localResourceLevelsProductionModifier[i][j] = baseProduction[j] / (1.0f + localResourceLevels[i]);
 				}
+				localResourcesDecision[i] = 0.0f;
+				inventoryResourcesDecision[i] = 0.0f;
+			}
+			for (int i = 0; i < DisplayForm.NaturalResourceTypesCount; i++)
+			{
+				baseProduction[i] = 0.0f;
 			}
 
+			// 
 			for (int i = DisplayForm.NaturalResourceTypesCount; i < totalResourceCount; i++)
 			{
 				if (random.Next() > random.Next())
 				{
 					baseCost[i] = (float)random.NextDouble();
+					// If derived resources are being used in this action, then some baseProduction is possible
+					for (int j = 0; j < DisplayForm.NaturalResourceTypesCount; j++)
+					{
+						if (random.Next() > random.Next())
+						{
+							baseProduction[j] += (1.0f - (float)random.NextDouble()) / totalResourceCount;
+						}
+					}
 				}
 				baseProduction[i] = 0.0f;
 				localResourceLevelsProductionModifier[i] = new MatrixMath.Vector(DisplayForm.NaturalResourceTypesCount);
